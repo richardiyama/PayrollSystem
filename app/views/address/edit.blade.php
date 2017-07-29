8dd3451f19aeaf445e55822ae33abbe058be7ac6
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-29 20:58:19 +0100

    Update edit.blade.php

--- app/views/address/edit.blade.php
+++ app/views/address/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.default')
 @section('content')
     <div class="row">
+        
         <div class="col-md-12">
             @include('includes.alert')
             <section class="panel">
