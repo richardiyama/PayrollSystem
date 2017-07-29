9d83da52a7ef9f0034edac3b2d201a3101260ecb
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-29 20:55:11 +0100

    Update SalaryRank.php

--- app/models/SalaryRank.php
+++ app/models/SalaryRank.php
@@ -4,6 +4,7 @@ class SalaryRank extends \Eloquent {
 	protected $fillable = [];
 	protected $table = 'salary_rank';
 
+	
 	// public function user() {
 	// 	return $this->belongsTo('User', 'user_id', 'id');
 	// }
